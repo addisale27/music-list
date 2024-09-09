6e76da8f9254ddbb871f1287c5f7d5e1d648267b
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-09-09 23:52:47 +0300

    action type fixed

--- actions/getAllPlayList.ts
+++ actions/getAllPlayList.ts
@@ -1,18 +1,16 @@
 import prisma from "@/libs/prismadb";
+import { PlayList, Review, User } from "@prisma/client"; // Import the necessary types
 
 export interface IParams {
   searchTerm?: string | null;
 }
 
-export default async function getAllMusicLists(params: IParams) {
+export default async function getAllMusicLists(
+  params: IParams
+): Promise<(PlayList & { reviews: (Review & { user: User })[] })[]> {
   try {
     const { searchTerm } = params;
-    let searchString = searchTerm;
-
-    // If no search term, set searchString to an empty string
-    if (!searchTerm) {
-      searchString = "";
-    }
+    let searchString = searchTerm ?? ""; // Default to an empty string if searchTerm is undefined or null
 
     const playLists = await prisma.playList.findMany({
       where: {
@@ -46,5 +44,6 @@ export default async function getAllMusicLists(params: IParams) {
     return playLists; // Return the result
   } catch (error) {
     console.error("Error fetching playlists:", error); // Log the error
+    throw error; // Re-throw the error for further handling
   }
 }
