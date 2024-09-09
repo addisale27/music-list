e5c48ae121cdd132578c288571e317888078d282
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-09-10 00:16:46 +0300

    Fix bug in getAllMusicLists function

--- actions/getAllPlayList.ts
+++ actions/getAllPlayList.ts
@@ -1,17 +1,15 @@
 import prisma from "@/libs/prismadb";
-import { PlayList, Review, User } from "@prisma/client";
 
 export interface IParams {
   searchTerm?: string | null;
 }
 
-export default async function getAllMusicLists(
-  params: IParams
-): Promise<(PlayList & { reviews: (Review & { user: User })[] })[]> {
+export default async function getAllMusicLists(params: IParams) {
   try {
     const { searchTerm } = params;
-    // Ensure searchString is a string
-    const searchString: string = searchTerm || "";
+
+    // Ensure searchString is always a string
+    const searchString = searchTerm ?? "";
 
     const playLists = await prisma.playList.findMany({
       where: {
@@ -44,7 +42,7 @@ export default async function getAllMusicLists(
 
     return playLists; // Return the result
   } catch (error) {
-    console.error("Error while fetching playlists:", error); // Log the error
-    throw error; // Re-throw the error for further handling
+    console.error("Error fetching playlists:", error); // Log the error
+    throw new Error("Error fetching playlists"); // Throw the error to indicate failure
   }
 }
