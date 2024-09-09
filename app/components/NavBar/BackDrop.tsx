d87b29c9528f99574bbff896c4eed97d2e4e567a
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-09-09 21:35:09 +0300

    fixed

--- app/components/NavBar/BackDrop.tsx
+++ app/components/NavBar/BackDrop.tsx
@@ -6,9 +6,9 @@ const BackDrop: React.FC<BackDropProps> = ({ onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="z-20 bg-slate-200 opacity-50 w-screen h-screen fixed top-0 left-0"
+      className="z-21 bg-slate-200 opacity-50 w-screen h-screen fixed top-0 left-0"
     ></div>
   );
 };
-
+//deployment
 export default BackDrop;
